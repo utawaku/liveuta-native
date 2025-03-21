784e89ede97d5dc211df1df40283b23d2e062fa0
pekochan069 <pekochan069@gmail.com>
2025-03-22 03:42:30 +0900

    Filesystem and localStorage errors

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -9,3 +9,18 @@ export class JSONParseError extends Data.TaggedError("JSONParseError")<{
   message?: string;
   cause?: unknown;
 }> {}
+
+export class FSReadError extends Data.TaggedError("FSReadError")<{
+  message?: string;
+  cause?: unknown;
+}> {}
+
+export class FSWriteError extends Data.TaggedError("FSWriteError")<{
+  message?: string;
+  cause?: unknown;
+}> {}
+
+export class LocalStorageError extends Data.TaggedError("LocalStorageError")<{
+  message?: string;
+  cause?: unknown;
+}> {}
