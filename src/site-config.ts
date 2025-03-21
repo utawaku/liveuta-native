0c7e92fc641f2de137697c59ba38d6b214ba66d2
pekochan069 <pekochan069@gmail.com>
2025-03-22 03:42:56 +0900

    User settings

---
+++ src/site-config.ts
@@ -0,0 +1,6 @@
+import { Settings } from "./types/settings";
+
+export const siteConfig: Settings = {
+  autoSync: true,
+  syncPeriod: 5,
+};
---
+++ src/types/settings.ts
@@ -0,0 +1,10 @@
+export type Settings = {
+  /**
+   * Automatically sync schedule
+   */
+  autoSync: boolean;
+  /**
+   * Sync period in minutes
+   */
+  syncPeriod: number;
+};
